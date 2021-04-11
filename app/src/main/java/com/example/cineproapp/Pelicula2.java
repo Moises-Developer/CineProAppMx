d26331ae984b7a834c81e63fc391712d305d4166
Moises-Developer <2120300369@soy.utj.edu.mx>
2021-04-10 19:24:38 -0500

    Fin del fronted parte 1

---
+++ app/src/main/java/com/example/cineproapp/Pelicula2.java
@@ -0,0 +1,14 @@
+package com.example.cineproapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pelicula2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pelicula2);
+    }
+}
