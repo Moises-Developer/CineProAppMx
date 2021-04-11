4475f523208fa083dee99fd3c28b5d9d321cf78e
Moises-Developer <2120300369@soy.utj.edu.mx>
2021-04-11 15:38:19 -0500

    Fronted Terminado

---
+++ app/src/main/java/com/example/cineproapp/ActualizarUsuario.java
@@ -0,0 +1,14 @@
+package com.example.cineproapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActualizarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_usuario);
+    }
+}
